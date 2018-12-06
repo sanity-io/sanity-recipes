a9a418a0fd357610a571d533bf5d78812b7d458f
Knut Melvær <knut.melvaer@gmail.com>
2018-12-06 14:41:25 +0200

    add example

---
+++ examples/links-in-portabletext/react-portableText.js
@@ -0,0 +1,27 @@
+import PortableText from '@sanity/block-content-to-react'
+
+const serializers = {
+  marks: {
+    internalLink: ({mark, children}) => {
+      const {type, slug = {}, url} = mark
+      const href = url ? url : `/${_type}/${slug.current}`
+      return <a href={href}>{children}</a>
+    },
+    externalLink: ({mark, children}) => {
+      // Read https://css-tricks.com/use-target_blank/
+      const { blank, url } = mark
+      return blank ?
+        <a href={url} target="_blank" rel="noopener">{children}</a>
+        : <a href={url}>{children}</a>
+    }
+  }
+}
+
+const BodyText = (props) => (
+  <PortableText
+    blocks={props.blocks}
+    serializers={serializers}
+  />
+  )
+
+export BodyText
