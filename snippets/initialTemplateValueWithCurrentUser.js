f92a987b6b391a8decd29629bffe304d83cf037a
Knut Melvær <knut@sanity.io>
2020-08-18 10:50:34 +0200

    Create initialTemplateValueWithCurrentUser.js
    A minimal example of how to include the current user in an initial value template

---
+++ snippets/initialTemplateValueWithCurrentUser.js
@@ -0,0 +1,38 @@
+// initialValueTemamplate.js
+
+/**
+  * Learn more about initial values on https://www.sanity.io/docs/initial-value-templates-api
+  */
+
+import T from '@sanity/base/initial-value-template-builder'
+import userStore from 'part:@sanity/base/user'
+
+export default {
+  ...T.defaults,
+  T.template({
+    id: 'newsStory',
+    title: 'News story',
+    schemaType: 'newsStory',
+    value: async () => {
+      const {name, id} = await userStore.getUser('me')
+      return {
+        author: {
+          name,
+          id,
+          _type: 'user'
+        },
+      }
+    }
+  })
+}
+
+/*
+Shape of the user object from userStore.getUser('me'):
+{
+  "id": "<user-id>",
+  "name": "Your Name",
+  "email": "your@email.com",
+  "profileImage": "https://url.to.loginprovider/image",
+  "role": "<project-role>",
+}
+*/
